2d32d42da20bc41e77bb130895a3cc4faf4a9d5b
vishnuchalil <vishnu.chalil@litmus7.com>
2021-06-14 23:18:42 +0530

    code imporvement

--- spa-with-react-router/src/App.js
+++ spa-with-react-router/src/App.js
@@ -1,34 +1,50 @@
+import React, { Suspense } from "react";
 import { Redirect, Route, Switch } from "react-router-dom";
 import Layout from "./components/layout/Layout";
-import AllQuotes from "./pages/AllQuotes";
-import NewQuote from "./pages/NewQuote";
-import Notfound from "./pages/NotFound";
-import QuoteDetail from "./pages/QuoteDetail";
+import LoadingSpinner from "./components/UI/LoadingSpinner";
+//import AllQuotes from "./pages/AllQuotes";
+//import NewQuote from "./pages/NewQuote";
+//import Notfound from "./pages/NotFound";
+
+
+const NewQuote = React.lazy(() => import("./pages/NewQuote"));
+const  QuoteDetail = React.lazy(() => import( "./pages/QuoteDetail")); 
+const  Notfound = React.lazy(() => import( "./pages/NotFound")); 
+const  AllQuotes = React.lazy(() => import( "./pages/AllQuotes")); 
+
 
 function App() {
 
- 
+
   return (
 
     <Layout>
+      <Suspense fallback={
+        <div className='centered'>
+            <LoadingSpinner/>
+        </div>
+      }>
 
-    <Switch>
-      <Route path="/" exact>
-        <Redirect to="/quotes" />
-      </Route>
-      <Route path="/quotes" exact>
-        <AllQuotes />
-      </Route>
-      <Route path="/quotes/:quoteId">
-        <QuoteDetail />
-      </Route>
-      <Route path='/new-quote'>
-        <NewQuote />
-      </Route>
-      <Route path="*">
-        <Notfound/>
-      </Route>
-    </Switch>
+        <Switch>
+          <Route path="/" exact>
+            <Redirect to="/quotes" />
+          </Route>
+          <Route path="/quotes" exact>
+            <AllQuotes />
+          </Route>
+          <Route path="/quotes/:quoteId">
+            <QuoteDetail />
+          </Route>
+          <Route path='/new-quote'>
+            <NewQuote />
+          </Route>
+          <Route path="*">
+            <Notfound />
+          </Route>
+        </Switch>
+
+      </Suspense>
+
     </Layout>
 
   );
